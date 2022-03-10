0deea91278edbcb03b18c409a683ea653d5c2284
Alex Mwangi <alex@stl-horizon.com>
2022-03-10 10:00:00 +0300

    Added @yield('content inside the <main> </main> tags

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -29,7 +29,7 @@
 
                          <div id="layoutSidenav_content">
                             <main>
-
+                                    @yield('content')
                             </main>
                                @include('layouts.inc.admin-footer')
                          </div>
