12684f8f2f9cd22d39d871e30cbe9f42e5e1e1a4
Alex Mwangi <alex@stl-horizon.com>
2022-03-10 17:18:30 +0300

    Added add category form

--- resources/views/Admin/Category/create.blade.php
+++ resources/views/Admin/Category/create.blade.php
@@ -6,14 +6,74 @@
 
 
  <div class="container-fluid px-4">
-                        <h1 class="mt-4">Add Category</h1>
-                        <ol class="breadcrumb mb-4">
-                            <li class="breadcrumb-item active">Category</li>
-                        </ol>
-                        <div class="row">
 
+                    <div class="card mt-4">
+                        <div class="card-header">
+                                <h4>Add Category</h4>
+                        </div>
+                        <div class="card-body">
 
+                            <form action=" {{ url('admin/add-category')}} " method="POST">
+                                @csrf
 
+                                <div class="form-group mb-3">
+                                    <label for="">Category Name</label>
+                                    <input type="text" name="name" id="" class="form-control">
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <label for="">Slug</label>
+                                    <input type="text" name="slug" id="" class="form-control">
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <label for="">Description</label>
+                                 <textarea name="description" id=""  rows="3" class="form-control"></textarea>
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <label for="">Image</label>
+                                    <input type="file" name="image" id="" class="form-control">
+                                </div>
+
+                                <h6>SEO Tags</h6>
+                                <div class="form-group mb-3">
+                                    <label for="">Meta title</label>
+                                    <input type="text" name="meta_title" id="" class="form-control">
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <label for="">Meta description</label>
+                                    <input type="text" name="meta_desc" id="" class="form-control">
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <label for="">Meta keyword</label>
+                                    <input type="text" name="meta_keyword" id="" class="form-control">
+                                </div>
+
+                                 <h6>Status Mode</h6>
+
+                                 <div class="row">                            
+
+                                  <div class="form-group col-md-3 mb-3">
+                                    <label for="">Navbar Status</label>
+                                    <input type="checkbox" name="navbar_status"  >
+                                </div>
+
+                                  <div class="form-group col-md-3 mb-3">
+                                    <label for="">Status</label>
+                                    <input type="checkbox" name="status" >
+                                </div>
+
+                                <div class="col-md-6">
+                                    <button type="submit" class="btn btn-primary">Save Category</button>
+                                </div>
+                             </div>
+
+                            </form>
+
+                        </div>
                     </div>
 
 
