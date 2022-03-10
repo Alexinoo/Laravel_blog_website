0fa76b4b50a6fb2f574d6af4038fc2e5fb512648
Alex Mwangi <alex@stl-horizon.com>
2022-03-10 16:02:10 +0300

    Added authenticated() - Redirect users/Admin to different routes Home/Admin Dashboard

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -27,7 +28,25 @@ class LoginController extends Controller
      * @var string
      */
 
+    //  Commented out to add Muli -redirects
+
     // protected $redirectTo = RouteServiceProvider::HOME;
+
+    // Added authenticated Function for redirects
+    public function authenticated(){
+
+        if(Auth::user() ->role_as == '1'){
+
+            // if Admin - redirect to Admin /dashboard
+            return redirect('admin/dashboard')->with('status','Welcome to Admin Dashboard');
+
+        }else{
+
+            //  redirect to homepage
+             return redirect('/home')->with('status','Logged In Successfully');
+
+        }
+    }
 
     /**
      * Create a new controller instance.
