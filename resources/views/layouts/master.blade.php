672ea8cd0130169f3689426e96041888243df265
Alex Mwangi <alex@stl-horizon.com>
2022-03-10 09:24:48 +0300

    Added bootstrap bundle and script js to master blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -21,5 +21,9 @@
 </head>
 <body>
 
+
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+          
+        <script src="js/scripts.js"></script>
     </body>
 </html>
