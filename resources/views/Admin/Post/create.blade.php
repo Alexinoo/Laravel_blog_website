b6d31c655403fa8ef07b2aab319bcb4ef2b66ab3
Alex Mwangi <alex@stl-horizon.com>
2022-03-14 15:31:44 +0300

    Created Post view directory

---
+++ resources/views/Admin/Post/create.blade.php
@@ -0,0 +1,88 @@
+@extends('layouts.master')
+
+@section('title', 'Add Post')
+
+@section('content')
+
+
+ <div class="container-fluid px-4">
+
+    <div class="card mt-2">
+
+        <div class="card-header">
+            <h4> Add Posts
+
+                <a href="{{url('admin/add-post')}}" class="btn btn-primary float-end">Add Posts</a>
+
+            </h4>
+        </div>
+
+        <div class="card-body">
+
+            <form action="{{ url('admin/add-post')}}" method="POST">
+
+                <div class="form-group mb-3">
+                    <label for=""> Category</label>
+                    <select name="" id="" class="form-control">
+                        <option value=""></option>
+                    </select>
+                </div>
+            
+                <div class="form-group mb-3">
+                    <label for="">Post Name</label>
+                    <input type="text" name="name" id="" class="form-control">
+                </div>
+
+                  <div class="form-group mb-3">
+                    <label for="">Slug</label>
+                    <input type="text" name="slug" id="" class="form-control">
+                </div>
+
+                 <div class="form-group mb-3">
+                    <label for="">Description</label>
+                   <textarea name="description" id=""rows="3" class="form-control"></textarea>
+                </div>
+
+                   <div class="form-group mb-3">
+                    <label for="">Youtube Iframe Link</label>
+                    <input type="text" name="yt_iframe" id="" class="form-control">
+                </div>
+
+                <h4>SEO Tags</h4>
+                <div class="form-group mb-3">
+                    <label for="">Meta Title</label>
+                    <input type="text" name="meta_title" id="" class="form-control">
+                </div>
+                <div class="form-group mb-3">
+                    <label for="">Meta Description</label>
+                   <textarea name="meta_description" id="" rows="3" class="form-control"></textarea>
+                </div>
+                <div class="form-group mb-3">
+                    <label for="">Meta Keyword</label>
+                   <textarea name="meta_keyword" id="" rows="3" class="form-control"></textarea>
+                </div>
+
+                <h4>Status</h4>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="mb-3">
+                            <label for="">Status</label>
+                            <input type="checkbox" name="status" id="">
+                        </div>
+                    </div>
+
+                    <div class="col-md-8">
+                        <div class="mb-3">
+                            <button type="submit" class="btn btn-primary float-end">Save Post</button>
+                        </div>
+                    </div>
+
+                </div>
+
+            </form>
+        </div>
+    </div>
+
+ </div>
+
+ @endsection
---
+++ resources/views/Admin/Post/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.master')
+
+@section('title', 'View Posts')
+
+@section('content')
+
+
+ <div class="container-fluid px-4">
+
+    <div class="card mt-2">
+
+        <div class="card-header">
+            <h4> View Posts
+
+                <a href="{{url('admin/add-post')}}" class="btn btn-primary float-end">Add Posts</a>
+
+            </h4>
+        </div>
+
+        <div class="card-body">
+            Your content
+        </div>
+    </div>
+
+ </div>
+
+ @endsection
