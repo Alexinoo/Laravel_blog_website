e9acba2289f304aa96ef0de2f06974c597f1c743
Alex Mwangi <alex@stl-horizon.com>
2022-03-14 16:10:54 +0300

    Added store method and saving Post logic after validation

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Http\Requests\Admin\PostFormRequest;
 use App\Models\Category;
 
 class PostController extends Controller
@@ -23,5 +24,24 @@ class PostController extends Controller
         return view('Admin.post.create', [
             'categories' => $categories
         ]);
+    }
+    public function store(PostFormRequest $request)
+    {
+        $data = $request->validated();
+
+        $model = new Post;
+
+        $model->category_id = $data['category_id'];
+        $model->name = $data['name'];
+        $model->slug = $data['slug'];
+        $model->description = $data['description'];
+        $model->yt_iframe = $data['yt_iframe'];
+        $model->meta_title = $data['meta_title'];
+        $model->meta_description = $data['meta_description'];
+        $model->meta_keyword = $data['meta_keyword'];
+        $model->status = $request->status == true ? 1 : 0;
+        $model->save();
+
+        return redirect('admin/posts')->with('status', 'Post Added Successfully');
     }
 }
