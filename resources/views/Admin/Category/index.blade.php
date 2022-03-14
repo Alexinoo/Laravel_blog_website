d7bea1cfceced6a7bdb676a218f52bcff8e2947d
Alex Mwangi <alex@stl-horizon.com>
2022-03-14 10:36:25 +0300

    Added an alert for saving successfuly

--- resources/views/Admin/Category/index.blade.php
+++ resources/views/Admin/Category/index.blade.php
@@ -7,6 +7,9 @@
 
  <div class="container-fluid px-4">
                         <h1 class="mt-4">Category</h1>
+                        @if(session('status'))
+                            <div class="alert alert-success">{{session('status')}}</div>
+                        @endif
                         <ol class="breadcrumb mb-4">
                             <li class="breadcrumb-item active">Category</li>
                         </ol>
