444fe83063489ef6c5946b47555df1d165b99a2c
Alex Mwangi <alex@stl-horizon.com>
2022-03-14 15:01:35 +0300

    Add add/view posts routes

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,10 @@ Route::prefix('admin')->middleware(['auth','isAdmin'])->group(function(){
     Route::put('update-category/{category_id}',[ App\Http\Controllers\Admin\CategoryController::class, 'update' ]);
 
     Route::get('delete-category/{category_id}',[ App\Http\Controllers\Admin\CategoryController::class, 'destroy' ]);
+
+
+      Route::get('posts',[ App\Http\Controllers\Admin\PostController::class, 'index' ]);
+      Route::get('add-post',[ App\Http\Controllers\Admin\PostController::class, 'index' ]);
  
 });
 
