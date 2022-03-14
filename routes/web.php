0a99d22b7fb96ea273f3eabb8749a2fa262c400d
Alex Mwangi <alex@stl-horizon.com>
2022-03-14 12:11:27 +0300

    Added route for edit

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,9 @@ Route::prefix('admin')->middleware(['auth','isAdmin'])->group(function(){
     Route::get('add-category',[ App\Http\Controllers\Admin\CategoryController::class, 'create' ]);
 
     Route::post('add-category',[ App\Http\Controllers\Admin\CategoryController::class, 'store' ]);
+
+
+    Route::get('edit-category/{category_id}',[ App\Http\Controllers\Admin\CategoryController::class, 'edit' ]);
  
 });
 
