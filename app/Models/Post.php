805ea397dd02e024c8b489a3b54204d1b9b1ba81
Alex Mwangi <alex@stl-horizon.com>
2022-03-14 16:47:34 +0300

    Added category function for defining relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
 
 class Post extends Model
 {
@@ -11,15 +12,20 @@ class Post extends Model
     protected $table = 'posts';
 
     protected $fillable = [
-            'category_id',
-            'name',
-            'slug',
-            'description',
-            'yt_iframe',
-            'meta_title',
-            'meta_description',
-            'meta_keyword',
-            'status',
-            'created_by'
+        'category_id',
+        'name',
+        'slug',
+        'description',
+        'yt_iframe',
+        'meta_title',
+        'meta_description',
+        'meta_keyword',
+        'status',
+        'created_by'
     ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
 }
