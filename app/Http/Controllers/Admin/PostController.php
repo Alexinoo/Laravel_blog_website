9f1db460bcd896fce58f5321917c047528b11c26
Alex Mwangi <alex@stl-horizon.com>
2022-03-14 15:51:28 +0300

    Fetch categories from Category model

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -4,17 +4,24 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class PostController extends Controller
 {
-    
-    public function index(){
+
+    public function index()
+    {
 
         return view('Admin.post.index');
     }
 
-    public function create(){
+    public function create()
+    {
 
-        return view('Admin.post.create');
+        $categories = Category::where(['status' => 0])->get();
+
+        return view('Admin.post.create', [
+            'categories' => $categories
+        ]);
     }
 }
