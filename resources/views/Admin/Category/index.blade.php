0db8ea7a4fa2765735e6670c5238773a9263f163
Alex Mwangi <alex@stl-horizon.com>
2022-03-14 11:41:54 +0300

    Modified to use card - loop through data passed from controller

--- resources/views/Admin/Category/index.blade.php
+++ resources/views/Admin/Category/index.blade.php
@@ -6,19 +6,52 @@
 
 
  <div class="container-fluid px-4">
-                        <h1 class="mt-4">Category</h1>
-                        @if(session('status'))
+
+    <div class="card mt-2" >
+
+        <div class="card-header">
+                     <h4 >View Category
+
+                        <a href="{{url('admin/add-category')}}" class="btn btn-primary btn-sm float-end">Add Category</a>
+                     </h4>
+        </div>
+        <div class="card-body">
+
+              @if(session('status'))
                             <div class="alert alert-success">{{session('status')}}</div>
                         @endif
-                        <ol class="breadcrumb mb-4">
-                            <li class="breadcrumb-item active">Category</li>
-                        </ol>
-                        <div class="row">
 
-
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Category Name</th>
+                                    <th>Image</th>
+                                    <th>Status</th>
+                                    <th>Edit</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($categories as $category)
+                                        <tr>
+                                             <td>{{$category->id }}</td>
+                                             <td>{{$category->name }}</td>
+                                             <td>
+                                                 <img src="{{ asset('uploads/category/'.$category->image)}}" alt="Img" width="70px" height="50px">
+                                            </td>
+                                             <td>{{$category->status == 1 ? 'Hidden' : 'Shown'}}</td>
+                                             <td>
+                                                 <a href="{{ url('admin/edit-category')}}" class="btn btn-primary btn-sm">Edit</a>
+                                             </td>
+                                </tr>
+                                @endforeach
+                            
+                            </tbody>
+                        </table>
 
                     </div>
-
+                </div>
+                    
 
               </div>    
 
