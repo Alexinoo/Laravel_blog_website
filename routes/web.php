bef481bd753b96cae68a21d94316f04f68acf548
Alex Mwangi <alex@stl-horizon.com>
2022-03-14 17:33:18 +0300

    Added update/edit route

--- routes/web.php
+++ routes/web.php
@@ -47,4 +47,9 @@ Route::prefix('admin')->middleware(['auth', 'isAdmin'])->group(function () {
 
     Route::get('add-post', [App\Http\Controllers\Admin\PostController::class, 'create']);
     Route::post('add-post', [App\Http\Controllers\Admin\PostController::class, 'store']);
+
+    Route::get('post/{post_id}', [App\Http\Controllers\Admin\PostController::class, 'edit']);
+
+
+    Route::put('update-post/{post_id}', [App\Http\Controllers\Admin\PostController::class, 'update']);
 });
