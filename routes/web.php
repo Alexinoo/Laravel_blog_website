25253d35a653f879687a34b461d48cfca512ae31
Alex Mwangi <alex@stl-horizon.com>
2022-03-15 21:54:52 +0300

    Modified/removed / and customezed it to home/frontend view

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,11 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', [App\Http\Controllers\Frontend\FrontendController::class, 'index']);
 
 // Set a prefix using prefix group function
 //admin/route    e.g admin/dashboard
