84665474729f7022484ee9421376fb815455ef79
Alex Mwangi <alex@stl-horizon.com>
2022-03-15 15:59:31 +0300

    Modified edit-user view

--- resources/views/Admin/Users/edit.blade.php
+++ resources/views/Admin/Users/edit.blade.php
@@ -26,23 +26,48 @@
                             @endforeach
                             </div>  
                             @endif
+            <div class="row">
+                <div class="col-md-12">         
 
-            <form action="{{ url('admin/update-user/'.$user->id)}}" method="POST">
-                @csrf
-                @method('put')
+                <div class="mb-3">
+                    <label >Full Name</label>
+                    <p class="form-control">{{ $user->name }}</p>
+                </div>
 
-              
+                <div class="mb-3">
+                    <label >Email ID </label>
+                    <p class="form-control">{{ $user->email }}</p>
+                </div>
 
-                    <div class="col-md-8">
+                <div class="mb-3">
+                    <label >Created At </label>
+                    <p class="form-control">{{ $user->created_at->format('d/m/Y') }}</p>
+                </div>
+
+                       <form action="{{ url('admin/update-user/'.$user->id)}}" method="POST">
+                        @csrf
+                        @method('put')
+
+                          <div class="mb-3">
+                            <label >Role As </label>
+                            <select name="" id="" class="form-control">
+                                <option value="1" {{ $user->role_as == 1 ? 'selected' : ''}}>Admin</option>
+                                <option value="0" {{ $user->role_as == 0 ? 'selected' : ''}}>User</option>
+                                <option value="2" {{ $user->role_as == 2 ? 'selected' : ''}}>Blogger</option>
+                            </select>
+                        </div>
+
+                           <div class="col-md-8">
                         <div class="mb-3">
                             <button type="submit" class="btn btn-primary float-end">Update user</button>
                         </div>
                     </div>
 
-                </div>
+                     </form>
 
-            </form>
+                </div>        
         </div>
+    </div>
     </div>
 
  </div>
