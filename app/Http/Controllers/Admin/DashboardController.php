288be7f2f41ec7b606da3f4cf4f0b960a6b550ea
Alex Mwangi <alex@stl-horizon.com>
2022-03-16 22:25:54 +0300

    Count Total Categories/Posts/Users/Admin   and renderered to dashboard view

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index(){
+    public function index()
+    {
 
-               return view('admin.dashboard');
+        // Count Statistics to display on Dashboard
+        $nCategoriesCount = Category::count();
+        $nPostsCount = Post::count();
+        $nUsersCount = User::where(['role_as' => 0])->count();
+        $nAdminCount = User::where(['role_as' => 1])->count();
+
+        return view('admin.dashboard', [
+            'categoriesCount' => $nCategoriesCount,
+            'postsCount' => $nPostsCount,
+            'usersCount' => $nUsersCount,
+            'adminCount' => $nAdminCount
+        ]);
     }
 }
