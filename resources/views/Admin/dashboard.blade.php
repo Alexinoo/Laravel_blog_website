a51a1db1d7a3d95f243e16f68d61a8c08853827f
Alex Mwangi <alex@stl-horizon.com>
2022-03-16 22:26:40 +0300

    Made statistics on Categories/Posts/Users/Admin

--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -12,37 +12,61 @@
                         <div class="row">
                                     <div class="col-xl-3 col-md-6">
                                         <div class="card bg-primary text-white mb-4">
-                                            <div class="card-body">Primary Card</div>
+                                            <div class="card-body">
+                                                Total Categories
+                                                <h5>{{ $categoriesCount}}</h5>
+                                            </div>
                                             <div class="card-footer d-flex align-items-center justify-content-between">
-                                                <a class="small text-white stretched-link" href="#">View Details</a>
+                                                <a class="small text-white stretched-link" 
+                                                href="{{ url('admin/category')}}">
+                                                    View Categories
+                                                </a>
                                                 <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                                             </div>
                                         </div>
                                     </div>
 
                                     <div class="col-xl-3 col-md-6">
-                                <div class="card bg-warning text-white mb-4">
-                                    <div class="card-body">Warning Card</div>
+                                <div class="card bg-info text-white mb-4">
+                                    <div class="card-body">
+                                        Total Posts
+                                         <h5>{{ $postsCount }}</h5>
+                                    </div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <a class="small text-white stretched-link"
+                                         href="{{ url('admin/posts')}}">
+                                         View Posts
+                                        </a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-warning text-white mb-4">
+                                    <div class="card-body">
+                                        Total Users
+                                         <h5>{{$usersCount }}</h5>
+                                    </div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link"
+                                         href="{{ url('admin/users')}}">
+                                            View Users
+                                        </a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-success text-white mb-4">
-                                    <div class="card-body">Success Card</div>
-                                    <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    <div class="card-body">
+                                        Total Admin
+                                         <h5>{{ $adminCount}}</h5>
                                     </div>
-                                </div>
-                            </div>
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-danger text-white mb-4">
-                                    <div class="card-body">Danger Card</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <a class="small text-white stretched-link"
+                                         href="{{ url('admin/users')}}">
+                                         View Users
+                                        </a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                                     </div>
                                 </div>
