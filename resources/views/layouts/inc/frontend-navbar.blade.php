484efbe78e82df6ff0eba422889379dad89fc2c1
Alex Mwangi <alex@stl-horizon.com>
2022-03-16 08:58:54 +0300

    Added navbar for displaying categories

--- resources/views/layouts/inc/frontend-navbar.blade.php
+++ resources/views/layouts/inc/frontend-navbar.blade.php
@@ -26,28 +26,41 @@
                     </button>
 
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li><hr class="dropdown-divider"></li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                    <li class="nav-item">
+                                    <a class="nav-link"  href="{{ url('/')}}">Home</a>
+                                    </li>                                 
+                                  
+                                    {{-- Commented for future use --}}
+                                    {{-- <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Dropdown
+                                    </a>
+                                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <li><a class="dropdown-item" href="#">Action</a></li>
+                                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                                        <li><hr class="dropdown-divider"></li>
+                                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                    </ul>
+                                    </li> --}}
+
+                                    {{-- Fetch categories - Hidden --}}
+                                    @php
+                                        $categories = App\Models\Category::where([
+                                            'navbar_status' => 0 ,
+                                           'status' => 0 
+                                            ])->get();
+                                    @endphp
+
+                                    @foreach($categories as $category)
+                                        
+                                    <li class="nav-item">
+                                    <a class="nav-link" href="{{ url('tutorial/'.$category->slug)}}">{{$category->name }}</a>
+                                    </li>                                    
+                                    @endforeach
+                                       
+                                   
                             </ul>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link disabled">Disabled</a>
-                            </li>
-                        </ul>
 
                         </div>
                 </div>
