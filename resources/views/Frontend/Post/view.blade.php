43234f0f8700433ce3332447128ad8ebc6d5ac64
Alex Mwangi <alex@stl-horizon.com>
2022-03-16 17:14:47 +0300

    Added SEO title and meta tags

--- resources/views/Frontend/Post/view.blade.php
+++ resources/views/Frontend/Post/view.blade.php
@@ -1,12 +1,18 @@
 @extends('layouts.app')
 
+{{-- SEO Tags --}}
+
+@section('title', " $post->meta_title")
+@section('meta_description', " $post->meta_description")
+@section('meta_keyword', " $post->meta_keyword")
+
 @section('content')
 
 <div class="py-4">
     <div class="container">
         <div class="row">
 
-            <div class="col-md-9">
+            <div class="col-md-8">
 
                    <div class="category-heading">
                     <h4>{!! $post->name !!}</h4>
@@ -24,7 +30,7 @@
                 </div>
             </div>
 
-              <div class="col-md-3">
+              <div class="col-md-4">
                 <div class="border p-2 my-2">
                     <h4>Advertising area</h4>
                 </div>
@@ -33,6 +39,22 @@
                 </div>
                 <div class="border p-2 my-2">
                     <h4>Advertising area</h4>
+                </div>
+
+                <div class="card mt-3">
+                    <div class="card-header">
+                        <h4>Latest Posts</h4>
+                    </div>
+                    <div class="card-body">
+                        @foreach($latest_posts as $latest_post)
+
+                        {{-- Direct to that tutorial --}}
+                        <a href="{{ url('tutorial/'.$latest_post->category->slug. '/'.$latest_post->slug)}}" class="text-decoration-none">
+                            <h6> > {{ $latest_post->name }}</h6>
+                        </a>
+                            
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/Frontend/index.blade.php
+++ resources/views/Frontend/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('title', "Blogging Website")
+@section('meta_description', "Blogging Website")
+@section('meta_keyword', "Blogging Website")
+
 @section('content')
     <h4>Hello Frontend</h4>
 @endsection
