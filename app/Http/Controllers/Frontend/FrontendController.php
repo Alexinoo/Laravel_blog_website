4c22a3fe0c405985cd32c1f88545a77584ecf731
Alex Mwangi <alex@stl-horizon.com>
2022-03-16 08:53:57 +0300

    Added viewCategoryPostmethod to fetch available categories and fetch related posts

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class FrontendController extends Controller
@@ -11,5 +13,30 @@ class FrontendController extends Controller
     {
 
         return view('Frontend.index');
+    }
+
+
+    public function viewCategoryPost($category_slug)
+    {
+
+        $category = Category::where([
+            'slug' => $category_slug,
+            'status' => 0
+        ])->first();
+
+        if ($category) {
+
+            $posts = Post::where([
+                'category_id' => $category->id,
+                'status' => 0
+            ])->get();
+
+            return view('Frontend.post.index', [
+                'posts' => $posts,
+                'category' => $category
+            ]);
+        } else {
+            return redirect('/');
+        }
     }
 }
