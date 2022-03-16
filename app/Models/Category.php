e41a974363b86b0d1b49e2c17bb8a8dbc80d9bd4
Alex Mwangi <alex@stl-horizon.com>
2022-03-16 22:42:27 +0300

    Added posts method to return hasMany R/ships with Post class

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,17 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Post;
 
 class Category extends Model
 {
     use HasFactory;
 
-    protected $table ='categories';
+    protected $table = 'categories';
 
     protected $fillable = [
-            'name','slug','description','image',
-            'meta_title','meta_description','meta_keyword',
-            'navbar_status','status','created_by'
+        'name', 'slug', 'description', 'image',
+        'meta_title', 'meta_description', 'meta_keyword',
+        'navbar_status', 'status', 'created_by'
     ];
 
+    // Has Many posts - Relationships
+    public function posts()
+    {
+        return $this->hasMany(Post::class, 'category_id', 'id');
+    }
 }
