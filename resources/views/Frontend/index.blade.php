dd3130d62ecb7a8e66f03f50ea9e8ac290ec54e4
Alex Mwangi <alex@stl-horizon.com>
2022-03-17 11:51:05 +0300

    Designed view to include carousel - Used cards

--- resources/views/Frontend/index.blade.php
+++ resources/views/Frontend/index.blade.php
@@ -5,5 +5,31 @@
 @section('meta_keyword', "Blogging Website")
 
 @section('content')
-    <h4>Hello Frontend</h4>
+
+<div class="bg-danger py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+
+               <div class="owl-carousel category-carousel owl-theme">
+
+                @foreach($categories as $category)    
+                   <div class="item">
+                       <a href="{{ url('tutorial/'.$category->slug)}}" class="text-decoration-none">                      
+                       <img src="{{ asset('uploads/category/'.$category->image)}}" alt="Image" class="w-100" />
+                       <div class="card ">
+                           <div class="card-body text-center">
+                               <h5 class="text-dark">{{ $category->name }} </h5>
+                           </div>
+                       </div>
+                        </a>
+                   </div>
+                    @endforeach
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
