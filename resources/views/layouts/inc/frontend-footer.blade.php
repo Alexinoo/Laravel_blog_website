5c66636ec9ea0606f7bcefbb2f4a7295dd649f6f
Alex Mwangi <alex@stl-horizon.com>
2022-03-17 15:06:47 +0300

    Designed footer and copyright

---
+++ resources/views/layouts/inc/frontend-footer.blade.php
@@ -0,0 +1,40 @@
+<div class="py-5 bg-dark text-white">
+    <div class="container">
+            <div class="row">
+            <div class="col-md-6">
+                <h5>Coding Pro</h5>
+                <div class="underline"></div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ratione impedit perspiciatis adipisci repellat debitis, repellendus consequuntur asperiores itaque iste!</p>
+            </div>
+
+
+            <div class="col-md-3">
+                <h5>Quick Links</h5>
+                <div class="underline"></div>
+                <div><a href="" class="text-white">Home</a></div>
+                <div><a href="" class="text-white">About Us</a></div>
+                <div><a href="" class="text-white">Contact Us</a></div>
+                <div><a href="" class="text-white">Need Promotion ?</a></div>
+            </div>
+            <div class="col-md-3">
+                 <h5>Follow Us on</h5>
+                <div class="underline"></div>
+                <div><a href="" class="text-white">Facebook</a></div>
+                <div><a href="" class="text-white">Instagram</a></div>
+                <div><a href="" class="text-white">LinkedIn</a></div>
+                <div><a href="" class="text-white">Twitter</a></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="py-2 bg-gray">
+    <div class="container text-center">
+        <p class="mb-0">
+            &copy; Copyright at 
+            <a href="">Coding Pro</a> 
+            All rights reserved.
+            Designed and Developed by Coding Pro {{ date(('d-m-Y'))}}
+        </p>
+    </div>
+</div>
