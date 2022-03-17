41bc8006b9dc7f95c8af7bc81e5c03ba9b98d487
Alex Mwangi <alex@stl-horizon.com>
2022-03-17 16:17:26 +0300

    Replaced Navbar with Coding Pro

--- resources/views/layouts/inc/frontend-navbar.blade.php
+++ resources/views/layouts/inc/frontend-navbar.blade.php
@@ -24,7 +24,7 @@
                 <div class="container">
 
                     <a href="" class="navbar-brand d-inline d-sm-inline d-md-none">
-                        Navbar
+                        Coding Pro
                     </a>
                 
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
